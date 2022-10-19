b3fa6f36e5e5acdd74f4add13461aa77237efc2f
pedromathias <pedrohmathias10@gmail.com>
2022-10-19 16:13:27 -0300

    criação da tabela categoria

---
+++ EcommerceAPI/src/main/java/br/org/serratec/model/Categoria.java
@@ -0,0 +1,47 @@
+package br.org.serratec.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity	
+@Table(name = "categoria")
+public class Categoria {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="id_categoria")
+	private Long id;
+	
+	@Column(name="nome", nullable=false, length=40)
+	private String nome;
+	
+	@Column(name = "descricao", nullable = true, length = 150)
+	private String descricao;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+}
