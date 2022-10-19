c7c60115d549da9a5479c6232b42618abbb4e65b
Fernando Carvalho <fernandocarvalho2304@gmail.com>
2022-10-19 16:59:18 -0300

    Criação Cliente Service, Repository e Controller
    Criado/Desenvolvido o código das Classes Cliente Service, Cliente Repository e Cliente Controller.

---
+++ EcommerceAPI/src/main/java/br/org/serratec/service/ClienteService.java
@@ -0,0 +1,43 @@
+package br.org.serratec.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.org.serratec.model.Cliente;
+import br.org.serratec.repository.ClienteRepository;
+
+@Service
+public class ClienteService {
+
+	@Autowired
+	private ClienteRepository clienteRepositorio;
+	
+	public List<Cliente> obterTodos(){
+		return clienteRepositorio.findAll();
+	}
+	
+	public Optional<Cliente> obterPorId(Long id){
+		Optional<Cliente> optCliente = clienteRepositorio.findById(id);
+		
+		return optCliente;
+	}
+	
+	public Cliente cadastrar(Cliente cliente) {
+		
+		return clienteRepositorio.save(cliente);
+	}
+	
+	public Cliente atualizar(Long id, Cliente cliente) {
+		
+		cliente.setIdCliente(id);
+		return clienteRepositorio.save(cliente);
+		
+	}
+	
+	public void deletar(Long id) {
+		clienteRepositorio.deleteById(id);
+	}
+}
--- EcommerceAPI/src/main/java/br/org/serratec/service/EcommerceAPIService.java
+++
@@ -1,5 +0,0 @@
-package br.org.serratec.service;
-
-public class EcommerceAPIService {
-
-}
