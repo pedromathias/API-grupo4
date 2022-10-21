7bf388dd0987cf2c10b684b8b2b8fd522278f687
Fernando Carvalho <fernandocarvalho2304@gmail.com>
2022-10-21 20:56:28 -0300

    Validação Categoria
    Validação Categoria

--- EcommerceAPI/src/main/java/br/org/serratec/model/Categoria.java
+++ EcommerceAPI/src/main/java/br/org/serratec/model/Categoria.java
@@ -7,7 +7,8 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 import javax.persistence.Table;
-
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
@@ -16,14 +17,18 @@ import com.fasterxml.jackson.annotation.JsonBackReference;
 @Entity	
 @Table(name = "categoria")
 public class Categoria {
+	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name="id_categoria")
 	private Long id;
 	
+	@NotBlank(message = "Preencha o nome")
+	@Size(max = 40, message = "Tamanho máximo 40 caracteres")
 	@Column(name="nome", nullable=false, length=40)
 	private String nome;
 	
+	@Size(max = 150, message = "Tamanho máximo 150 caracteres")
 	@Column(name = "descricao", nullable = true, length = 150)
 	private String descricao;
 	
