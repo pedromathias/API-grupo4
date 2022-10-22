d22146c1e26e35e6977b17de3e236d40d1d4af72
Fernando Carvalho <fernandocarvalho2304@gmail.com>
2022-10-22 15:38:13 -0300

    PedidoDTO
    Criado o todas as classes relacionadas a PedidoDTO;

--- EcommerceAPI/src/main/java/br/org/serratec/service/PedidoService.java
+++ EcommerceAPI/src/main/java/br/org/serratec/service/PedidoService.java
@@ -1,13 +1,17 @@
 package br.org.serratec.service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import br.org.serratec.model.Pedido;
 import br.org.serratec.repository.PedidoRepository;
+import br.org.serratec.dto.PedidoRequestDTO;
+import br.org.serratec.dto.PedidoResponseDTO;
 import br.org.serratec.exception.ResourceNotFoundException;
 
 
@@ -17,33 +21,42 @@ public class PedidoService {
 	@Autowired
 	private PedidoRepository repositorio;
 	
-	public List<Pedido> obterTodos() {
-		return repositorio.findAll();
+	private ModelMapper mapper = new ModelMapper();
+	
+	public List<PedidoResponseDTO> obterTodos() {
+		List<Pedido> lista = repositorio.findAll();
+		var novaLista = new ArrayList<PedidoResponseDTO>();
+		for(Pedido pedido : lista) {
+		novaLista.add(mapper.map(pedido, PedidoResponseDTO.class));
+		}
+		return novaLista;
 	}
 	
-	public Optional<Pedido> obterPorId(Long id){
-		
+	public Optional<PedidoResponseDTO> obterPorId(Long id){
 		Optional<Pedido> optPedido = repositorio.findById(id);
-		
 		if(optPedido.isEmpty()) {
 			throw new ResourceNotFoundException("Não foi possivel encontrar o pedido com id " + id);
 		}
-		
-		return optPedido;
+		PedidoResponseDTO pedidoDTO = mapper.map(optPedido.get(), PedidoResponseDTO.class);
+		return Optional.of(pedidoDTO);
 	}
 	
-	public Pedido cadastrar(Pedido pedido) {
-		
-		pedido.setId(null);
-		return repositorio.save(pedido);
+	public PedidoResponseDTO cadastrar(PedidoRequestDTO pedido) {
+		var pedidoModel = mapper.map(pedido, Pedido.class);
+		pedidoModel.setId(null);
+		pedidoModel = repositorio.save(pedidoModel);
+		var response = mapper.map(pedidoModel, PedidoResponseDTO.class);
+		//colocar email
+		return response;
 	}
 	
-	public Pedido atualizar(Long id, Pedido pedido) {
+	public PedidoResponseDTO atualizar(Long id, PedidoRequestDTO pedido) {
 		
 		obterPorId(id);
-		
-		pedido.setId(id);
-		return repositorio.save(pedido);
+		var pedidoModel = mapper.map(pedido, Pedido.class);
+		pedidoModel.setId(id);
+		pedidoModel = repositorio.save(pedidoModel);
+		return mapper.map(pedidoModel, PedidoResponseDTO.class);
 	}
 	
 	public void deletar(Long id) {
